15eff98a29382f564c91d01851458d941de17c98
Master <thetruemaster801@gmail.com>
2017-05-30 09:23:48 -0500

    Remove ReTweakLoadController
    Signed-off-by: Master <thetruemaster801@gmail.com>

---
+++ src/main/java/org/slave/minecraft/retweak/loading/mod/ReTweakModController.java
@@ -0,0 +1,21 @@
+package org.slave.minecraft.retweak.loading.mod;
+
+import cpw.mods.fml.common.eventhandler.EventBus;
+
+/**
+ * <p>
+ *     {@link cpw.mods.fml.common.LoadController}
+ * </p>
+ *
+ * Created by Master on 5/30/2017 at 9:21 AM.
+ *
+ * @author Master
+ */
+public final class ReTweakModController {
+
+    private EventBus masterController;
+
+    ReTweakModController() {
+    }
+
+}
